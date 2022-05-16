a508048fa15062f1392e025849454e1c029e0220
BaironMugeni <105646560+BaironMugeni@users.noreply.github.com>
2022-05-16 15:30:56 +0300

    Add files via upload

---
+++ data/LacustrineProtectiveArea_2.js
@@ -0,0 +1,8 @@
+var json_LacustrineProtectiveArea_2 = {
+"type": "FeatureCollection",
+"name": "LacustrineProtectiveArea_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Bulago" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 32.651191645768485, -0.084986202544223 ], [ 32.633147733009622, -0.031246723240662 ], [ 32.588037951112476, 0.03426052525346 ], [ 32.634716768901697, 0.150369181266993 ], [ 32.655114235498672, 0.074270940501367 ], [ 32.7543557556724, 0.051127661093264 ], [ 32.695124650746578, -0.037130607835942 ], [ 32.651191645768485, -0.084986202544223 ] ] ] ] } }
+]
+}
